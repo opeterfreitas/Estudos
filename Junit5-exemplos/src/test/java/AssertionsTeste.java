0f3d4a5d0eef6c5dfbbfc05d62c3466e6292bc68
Peter Freitas <opeterfreitas@gmail.com>
2023-04-16 16:29:47 -0300

    Mais algumas assercoes

---
+++ Junit5-exemplos/src/test/java/AssertionsTeste.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AssertionsTeste {
+
+    @Test
+    void validarLancamentos() {
+
+        int[] primeiroLancamento = {10, 20, 30, 40, 50};
+        int[] segundoLancamento = {10, 20, 30, 40, 50};
+
+        assertArrayEquals(primeiroLancamento, segundoLancamento);
+    }
+
+    @Test
+    void validarSeObjetoEstaNulo() {
+
+        Pessoa pessoa = null;
+        assertNull(pessoa);
+
+        pessoa = new Pessoa("Peter", LocalDateTime.now());
+        assertNotNull(pessoa);
+    }
+
+    @Test
+    void validarNumerosDeTiposDiferentes() {
+
+        double valor = 5.0;
+        double outroValor = 5.0;
+
+        assertEquals(valor, outroValor);
+    }
+}
