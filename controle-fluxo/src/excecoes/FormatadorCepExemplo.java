ea9019c30d57763fb8e7c0ae9aeb54e486b16847
Peter Freitas <opeterfreitas@gmail.com>
2023-05-01 13:00:08 -0300

    Controle de fluxo - Excecoes

---
+++ controle-fluxo/src/excecoes/FormatadorCepExemplo.java
@@ -0,0 +1,20 @@
+package excecoes;
+
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("237650");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("O cep não corresponde com as regras de negócio");
+        }
+    }
+
+    static String formatarCep(String cep) throws CepInvalidoException {
+        if (cep.length() != 8)
+            throw new CepInvalidoException();
+
+        //simulando um cep formatado
+        return "23.765-064";
+    }
+}
